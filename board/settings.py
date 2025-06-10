87b11a4d69e56ea59003e57a72c31c56c4ae3109
panix0612 <41118121@gcloud.csu.edu.tw>
2025-06-10 15:55:16 +0800

    settings.py

--- board/settings.py
+++ board/settings.py
@@ -48,6 +48,7 @@ CAPTCHA_NOISE_FUNCTIONS = (
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
